c1bac6742befec3471b11732554f6c9d7bc3128b
Ifiora-Timothy <ifioratimothy2001@gmail.com>
2024-02-18 22:24:23 +0100

    these seems to contain bugs

---
+++ src/app/newpet/page.tsx
@@ -0,0 +1,22 @@
+import Form from "../components/Form";
+
+const NewPet = () => {
+  const petForm = {
+    name: "",
+    owner_name: "",
+    species: "",
+    age: 0,
+    poddy_trained: false,
+    diet: [],
+    image_url: "",
+    likes: [],
+    dislikes: [],
+  };
+
+  // return <Form formId="add-pet-form" petForm={petForm} />;
+  return (
+    <div>new pet</div>
+  )
+};
+
+export default NewPet;
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,7 +1,10 @@
 import {publicProcedure, router} from './trpc'
 
 export const appRouter=router({
-test:publicProcedure.query(()=>{return "Hello World!"})
+authCallback:publicProcedure.query(()=>{
+    
+})
+
 });
 
 export type AppRouter = typeof appRouter;
